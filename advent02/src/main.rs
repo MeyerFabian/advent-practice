25170112d0429cf5e08a445340d4b79b18f0c23f
MeyerFabian <fabian.meyer1337@web.de>
2017-12-02 23:13:14 +0100

    rustfmt

--- advent02/src/main.rs
+++ advent02/src/main.rs
@@ -4,11 +4,14 @@ use std::io::BufReader;
 fn main() {
     let file = File::open("checksum.txt").expect("Unable to open");
     let reader = BufReader::new(file);
-    let mut sum =0;
-    let mut sum2 =0;
-    for line in reader.lines(){
-        let split:Vec<i32> = line.unwrap().split("\t").map(|d| d.parse().unwrap()).collect(); 
-        sum +=sum_max_min(&split); 
+    let mut sum = 0;
+    let mut sum2 = 0;
+    for line in reader.lines() {
+        let split: Vec<i32> = line.unwrap()
+            .split("\t")
+            .map(|d| d.parse().unwrap())
+            .collect();
+        sum += sum_max_min(&split);
         sum2 += sum_even_divided(&split);
     }
 
@@ -18,7 +21,8 @@ fn main() {
 }
 
 // The spreadsheet consists of rows of apparently-random numbers.
-// To make sure the recovery process is on the right track, they need you to calculate the spreadsheet's checksum.
+// To make sure the recovery process is on the right track,
+// they need you to calculate the spreadsheet's checksum.
 // For each row, determine the difference between the largest value and the smallest value;
 // the checksum is the sum of all of these differences.
 
@@ -34,11 +38,15 @@ fn main() {
 
 //     In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
 
-fn sum_max_min(data:&Vec<i32>)-> i32{
+fn sum_max_min(data: &Vec<i32>) -> i32 {
     return data.iter().max().unwrap() - data.iter().min().unwrap();
 }
 
-// It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
+// It sounds like the goal is to find the only two numbers in each row
+// where one evenly divides the other - that is,
+// where the result of the division operation is a whole number.
+// They would like you to find those numbers on each line, divide them,
+// and add up each line's result.
 
 //     For example, given the following spreadsheet:
 
@@ -46,24 +54,24 @@ fn sum_max_min(data:&Vec<i32>)-> i32{
 //     9 4 7 3
 //     3 8 6 5
 
-//     In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+//     In the first row, the only two numbers that evenly divide are 8 and 2;
+//     the result of this division is 4.
 //     In the second row, the two numbers are 9 and 3; the result is 3.
 //     In the third row, the result is 2.
 
 //     In this example, the sum of the results would be 4 + 3 + 2 = 9.
 
-fn sum_even_divided(data:&Vec<i32>)-> i32{
-    let mut sum2 =0;
-    for  (i,c) in data.iter().enumerate(){
-        for a in data.iter().skip(i+1){
-            if c%a==0 {
-                sum2 += c/a;
+fn sum_even_divided(data: &Vec<i32>) -> i32 {
+    let mut sum2 = 0;
+    for (i, c) in data.iter().enumerate() {
+        for a in data.iter().skip(i + 1) {
+            if c % a == 0 {
+                sum2 += c / a;
             }
-            if a%c==0 {
-                sum2 += a/c;
+            if a % c == 0 {
+                sum2 += a / c;
             }
         }
     }
     return sum2;
 }
-
