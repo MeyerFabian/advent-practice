aeab9d62341a42fa820a0ab31eca7aabd36551a3
MeyerFabian <fabian.meyer1337@web.de>
2017-12-08 07:31:21 +0100

    removed unnessary contents

--- advent07/src/main.rs
+++ advent07/src/main.rs
@@ -68,6 +68,31 @@ fn find_root_node<'a>(names: &[&'a str], flat_children: &[&&str]) -> Option<(usi
     None
 }
 
+// The programs explain the situation: they can't get down. Rather, they could get down,
+// if they weren't expending all of their energy trying to keep the tower balanced.
+// Apparently, one program has the wrong weight, and until it's fixed, they're stuck here.
+
+// For any program holding a disc, each program standing on that disc forms a sub-tower.
+// Each of those sub-towers are supposed to be the same weight, or the disc itself isn't balanced.
+// The weight of a tower is the sum of the weights of the programs in that tower.
+
+// In the example above, this means that for ugml's disc to be balanced, gyxo, ebii,
+// and jptl must all have the same weight, and they do: 61.
+
+// However, for tknk to be balanced, each of the programs standing on its disc and
+// all programs above it must each match. This means that the following sums must all be the same:
+
+//     ugml + (gyxo + ebii + jptl) = 68 + (61 + 61 + 61) = 251
+//     padx + (pbga + havc + qoyq) = 45 + (66 + 66 + 66) = 243
+//     fwft + (ktlj + cntj + xhth) = 72 + (57 + 57 + 57) = 243
+
+// As you can see, tknk's disc is unbalanced: ugml's stack is heavier than the other two.
+// Even though the nodes above ugml are balanced, ugml itself is too heavy:
+// it needs to be 8 units lighter for its stack to weigh 243 and keep the towers balanced.
+// If this change were made, its weight would be 60.
+
+// Given that exactly one program is the wrong weight,
+// what would its weight need to be to balance the entire tower?
 
 fn error_recovery(
     names: &[&str],
@@ -105,29 +130,13 @@ fn error_recovery(
     }
 }
 
-fn traverse(
-    names: &[&str],
-    mut weights: &mut [u32],
-    children: &Vec<Vec<&str>>,
-    ind: usize,
-    childs_of_this: &mut Vec<u32>,
-) -> u32 {
-    let mut childs_of_all_childs: Vec<Vec<u32>> = vec![];
+fn traverse(names: &[&str], mut weights: &mut [u32], children: &Vec<Vec<&str>>, ind: usize) -> u32 {
+    //let mut childs_of_all_childs: Vec<Vec<u32>> = vec![];
     let mut sums: Vec<u32> = vec![];
     for child in children[ind].iter() {
-        let mut childs_of_this_child: Vec<u32> = vec![];
         let (i, _) = names.iter().enumerate().find(|&(_, d)| d == child).unwrap();
-        let val = traverse(
-            &names,
-            &mut weights,
-            &children,
-            i,
-            &mut childs_of_this_child,
-        );
-        childs_of_this.push(val);
-        //let sum:u32 =  childs_of_this_child.iter().sum();
+        let val = traverse(&names, &mut weights, &children, i);
         sums.push(val);
-        childs_of_all_childs.push(childs_of_this_child);
     }
     if sums.len() > 2 {
         error_recovery(&names, &mut weights, &mut sums, 0, ind, &children[ind]);
@@ -146,15 +155,7 @@ fn traverse_tree(
     children: &Vec<Vec<&str>>,
     root_ind: usize,
 ) {
-    let mut child_weights = vec![];
-
-    traverse(
-        &names,
-        &mut weights,
-        &children,
-        root_ind,
-        &mut child_weights,
-    );
+    traverse(&names, &mut weights, &children, root_ind);
 }
 fn main() {
     let mut file = File::open("input.txt").expect("Unable to open");
