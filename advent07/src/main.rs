c9291b38d5d401ee92038eac9bf90f54003926e2
MeyerFabian <fabian.meyer1337@web.de>
2017-12-08 07:21:56 +0100

    puh trees are really hard to come by because of borrowing.traversed in the vector structures.

--- advent07/src/main.rs
+++ advent07/src/main.rs
@@ -58,17 +58,104 @@ use std::io::Read;
 
 // Before you're ready to help them, you need to make sure your information is correct.
 // What is the name of the bottom program?
-
-fn find_root_node<'a>(names: &[&'a str], flat_children: &[&&str]) -> Option<&'a str> {
-    for node in names {
+fn find_root_node<'a>(names: &[&'a str], flat_children: &[&&str]) -> Option<(usize, &'a str)> {
+    for (i, node) in names.iter().enumerate() {
         let found = flat_children.iter().find(|&&children| node == children);
         if found == None {
-            return Some(node);
+            return Some((i, node));
         }
     }
     None
 }
 
+
+fn error_recovery(
+    names: &[&str],
+    weights: &mut [u32],
+    sums: &mut [u32],
+    try: usize,
+    ind: usize,
+    children: &[&str],
+) {
+    let filter: Vec<u32> = sums.iter()
+        .filter(|&&x| x != sums[try])
+        .map(|x| *x)
+        .collect();
+    if filter.len() == 1 {
+        println!("Identified error in {}:", names[ind]);
+        println!("childs:\t\t{:?}", sums);
+        let (i, _) = sums.iter()
+            .enumerate()
+            .find(|&(_, &x)| x == filter[try])
+            .unwrap();
+        let error_name = children[i];
+        println!("about name:\t{}", error_name);
+        let (j, _) = names
+            .iter()
+            .enumerate()
+            .find(|&(_, &d)| d == error_name)
+            .unwrap();
+        println!("with weight:\t{}", weights[j]);
+        weights[j] += sums[try] - sums[i];
+        sums[i] = sums[try];
+        println!("Recovered from error!");
+        println!("New weight:\t{}", weights[j]);
+        println!("Childs now:\t{:?}", sums);
+        println!();
+    }
+}
+
+fn traverse(
+    names: &[&str],
+    mut weights: &mut [u32],
+    children: &Vec<Vec<&str>>,
+    ind: usize,
+    childs_of_this: &mut Vec<u32>,
+) -> u32 {
+    let mut childs_of_all_childs: Vec<Vec<u32>> = vec![];
+    let mut sums: Vec<u32> = vec![];
+    for child in children[ind].iter() {
+        let mut childs_of_this_child: Vec<u32> = vec![];
+        let (i, _) = names.iter().enumerate().find(|&(_, d)| d == child).unwrap();
+        let val = traverse(
+            &names,
+            &mut weights,
+            &children,
+            i,
+            &mut childs_of_this_child,
+        );
+        childs_of_this.push(val);
+        //let sum:u32 =  childs_of_this_child.iter().sum();
+        sums.push(val);
+        childs_of_all_childs.push(childs_of_this_child);
+    }
+    if sums.len() > 2 {
+        error_recovery(&names, &mut weights, &mut sums, 0, ind, &children[ind]);
+        error_recovery(&names, &mut weights, &mut sums, 1, ind, &children[ind]);
+    }
+
+    let mut sum: u32 = 0;
+    if sums.len() > 0 {
+        sum = sums.iter().sum();
+    }
+    sum + weights[ind]
+}
+fn traverse_tree(
+    names: &[&str],
+    mut weights: &mut [u32],
+    children: &Vec<Vec<&str>>,
+    root_ind: usize,
+) {
+    let mut child_weights = vec![];
+
+    traverse(
+        &names,
+        &mut weights,
+        &children,
+        root_ind,
+        &mut child_weights,
+    );
+}
 fn main() {
     let mut file = File::open("input.txt").expect("Unable to open");
     let mut contents = String::new();
@@ -80,7 +167,7 @@ fn main() {
 
     let mut names = Vec::new();
 
-    let mut weights = Vec::new();
+    let mut weights: Vec<u32> = Vec::new();
     let children: Vec<Vec<&str>> = filter
         .lines()
         .map(|line| {
@@ -88,15 +175,21 @@ fn main() {
                 .filter(|&d| d != "->")
                 .collect::<Vec<&str>>();
             names.push(split_line[0]);
-            weights.push(split_line[1]);
+            weights.push(split_line[1].parse().unwrap());
             split_line.iter().skip(2).map(|x| *x).collect::<Vec<&str>>()
         })
         .collect();
 
     let flat_children: Vec<&&str> = children.iter().flat_map(|x| x).collect();
     let root_node = find_root_node(&names, &flat_children);
+    println!();
     match root_node {
-        Some(root) => println!("{}", root),
+        Some((i, root)) => {
+            println!("Root node is: {}!", root);
+            println!();
+            println!("Traversing Tree:");
+            traverse_tree(&names, &mut weights, &children, i);
+        }
         None => println!("No root node identified!"),
     };
 }
